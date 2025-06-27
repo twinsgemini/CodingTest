cfa7855dd68ae3c706304068df8a07eed60c54e1
twinsgemini <r_sharon@naver.com>
2025-06-27 11:32:29 +0900

    코딩테스트 연습 짝지어제거하기

--- src/main/java/problem/짝지어제거하기.java
+++ src/main/java/problem/짝지어제거하기.java
@@ -1,5 +1,7 @@
 package main.java.problem;
 
+import java.util.Stack;
+
 /**
  * 문제 설명
  * 짝지어 제거하기는, 알파벳 소문자로 이루어진 문자열을 가지고 시작합니다. 먼저 문자열에서 같은 알파벳이 2개 붙어 있는 짝을 찾습니다.
@@ -15,45 +17,31 @@ package main.java.problem;
  * 문자열의 길이 : 1,000,000이하의 자연수
  * 문자열은 모두 소문자로 이루어져 있습니다.
  */
-public class 실패_짝지어제거하기 {
+public class 짝지어제거하기 {
     public int solution(String s) {
-        return removeCouple(s) ? 1 : 0;
-    }
+        Stack<Character> stack = new Stack();
+        char[] charArray = s.toCharArray();
 
-    public boolean removeCouple(String s) {
-        StringBuffer sb = new StringBuffer();
-        boolean repeat = false;
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
 
-        if (s.length() == 0) return true;
-        if (s.length() == 1) return false;
-
-        // 짝 제거
-        for(int i = 0; i < s.length() - 1; i++) {
-            char c1 = s.charAt(i);
-            char c2 = s.charAt(i+1);
-
-            // 짝이면 제거하고 repeat = true
-            if (c1 == c2) {
-                repeat = true;
-                if (i+2 < s.length()) {
-                    sb.append(s.substring(i+2));
-                }
-                break;
+            if (stack.isEmpty()) {
+                stack.push(c);
             } else {
-                sb.append(s.substring(i, i+1));
+                char peek = stack.peek();
+                if (peek == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
             }
         }
 
-        // 확인 후 재호출
-        if (repeat == true) {
-            return removeCouple(sb.toString());
-        } else {
-            return false;
-        }
+        return stack.isEmpty() ? 1 : 0;
     }
 
     public static void main(String[] args) {
-        실패_짝지어제거하기 o = new 실패_짝지어제거하기();
+        짝지어제거하기 o = new 짝지어제거하기();
 
         System.out.println(o.solution("bbaabbaaccdd"));
         System.out.println(o.solution("bbaabbaacdcd"));
